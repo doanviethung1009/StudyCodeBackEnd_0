6022de8080b66a9c4dd6db178688bc3b5e51a642
hung doan viet <doanviethung1009@gmail.com>
2024-10-19 02:14:37 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ require('dotenv').config() // import dotenv
 const express = require('express') //import express
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-
-
+// Get the client
+const mysql = require('mysql2');
 
 const app = express() // tạo express application
-const port = process.env.DB_PORT || 8888; // init port
-const hostname =  process.env.DB_HOST // init hostname
+const port = process.env.BE_PORT || 8888; // init port
+const hostname =  process.env.BE_HOST // init hostname
 
 
 // console.log('check env', process.env)
@@ -16,7 +16,24 @@ const hostname =  process.env.DB_HOST // init hostname
 //config template ejs engine 
 configViewEngine(app);
 
+//test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, //defaul: 3306
+  user: 'root',
+  password: '123456', // set your password here || defaul: empty
+  database: 'hoidanit',
+});
 
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM USERS',
+  function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    //console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
 
 
 
